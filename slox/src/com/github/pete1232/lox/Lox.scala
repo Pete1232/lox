045e5af002fd622481022059cd2c2c8221488668
petenewman <peter.newman@disneystreaming.com>
2022-03-09 22:56:46 +0000

    Error handling

--- slox/src/com/github/pete1232/lox/Lox.scala
+++ slox/src/com/github/pete1232/lox/Lox.scala
@@ -1,37 +1,46 @@
 package com.github.pete1232.lox
 
-import cats.effect.{IO, IOApp}
-import cats.effect.ExitCode
+import cats.effect.{ExitCode, IO, IOApp}
 import cats.effect.kernel.Resource
 import cats.implicits.*
 import scala.io.Source
-import scala.util.*
 
 import java.io.EOFException
-import java.nio.file.Files
-import java.nio.file.Path
+import java.nio.file.{Files, NoSuchFileException, Path}
 
 object Lox extends IOApp:
   final def run(args: List[String]): IO[ExitCode] =
     args match
-      case Nil       => runPrompt().map(_ => ExitCode.Success)
-      case hd :: Nil => runFile(hd).map(_ => ExitCode.Success)
-      case _ => IO.println("Usage: slox [script]").map(_ => ExitCode(64))
+      case Nil       => runPrompt()
+      case hd :: Nil => runFile(hd)
+      case _ => IO.println("Usage: slox [script]").map(_ => ExitCode.Error)
 
-  private def runFile(path: String): IO[Unit] =
+  private def runFile(path: String): IO[ExitCode] =
     IO
       .blocking(Files.readString(Path.of(path)))
       .flatMap(runScan)
+      .map(_ => ExitCode.Success)
+      .recoverWith(ErrorHandler.file)
 
-  private def runPrompt(): IO[Unit] =
+  private def runPrompt(): IO[ExitCode] =
     (for
       _ <- IO.print("> ")
       l <- IO.readLine
       _ <- runScan(l)
       _ <- runPrompt()
-    yield (): Unit).recoverWith { case e: EOFException => IO.println(":quit") }
+    yield ExitCode.Success).recoverWith(ErrorHandler.repl)
 
-  private def runScan(source: String): IO[Unit] = {
+  private def runScan(source: String): IO[Unit] =
     // todo for now just printing the tokens
     Scanner(source).tokens.map(IO.println).sequence.map(_ => (): Unit)
-  }
+
+  object ErrorHandler:
+
+    val repl: PartialFunction[Throwable, IO[ExitCode]] = {
+      case _: EOFException => IO.println(":quit").map(_ => ExitCode.Success)
+    }
+
+    val file: PartialFunction[Throwable, IO[ExitCode]] = {
+      case _: NoSuchFileException =>
+        IO.println(":file not found").map(_ => ExitCode.Error)
+    }
