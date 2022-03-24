65e1d51b544ad6865079d02e053b3b9d68720912
petenewman <peter.newman@disneystreaming.com>
2022-03-24 16:15:32 +0000

    Simple expression types

--- slox/src/com/github/pete1232/lox/Expression.scala
+++ slox/src/com/github/pete1232/lox/Expression.scala
@@ -4,8 +4,23 @@ import com.github.pete1232.lox.Token.OperatorToken
 
 sealed trait Expression
 
-final case class Binary(
-    left: Expression,
-    operator: OperatorToken,
-    right: Expression,
-)
+object Expression:
+
+  final case class Literal(
+      value: Token.LiteralNumber | Token.LiteralString | Token.SimpleToken
+  ) extends Expression
+
+  final case class Group(
+      expression: Expression
+  ) extends Expression
+
+  final case class Unary(
+      operator: OperatorToken,
+      right: Expression,
+  ) extends Expression
+
+  final case class Binary(
+      left: Expression,
+      operator: Token.OperatorToken,
+      right: Expression,
+  ) extends Expression
