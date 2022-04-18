b574b9495dd973ce5a680e6539afced1fd75190b
petenewman <peter.newman@disneystreaming.com>
2022-04-18 19:11:34 +0100

    Generic interpreter

--- slox/src/com/github/pete1232/lox/Interpreter.scala
+++ slox/src/com/github/pete1232/lox/Interpreter.scala
@@ -5,43 +5,47 @@ import com.github.pete1232.lox.io.Logging
 import com.github.pete1232.lox.models.{Expression, Token}
 
 import cats.effect.IO
+import cats.effect.kernel.Sync
 import org.typelevel.log4cats.Logger
 
-trait Interpreter[T]:
-  extension (t: T) def interpret: IO[Either[InterpreterError, LoxValue]]
+sealed trait Interpreter[F[_], T]:
+  extension (t: T) def interpret: F[Either[InterpreterError, LoxValue]]
 
-object Interpreter extends Logging:
-
-  given Interpreter[Expression] with
-    extension (expr: Expression)
-
-      private def impl(using
-          Logger[IO]
-      ): IO[Either[InterpreterError, LoxValue]] = expr match
-        case l: Expression.Literal =>
-          Logger[IO].trace(s"Interpreting literal ${l.show}").as(Right(l.value))
-        case g: Expression.Group   =>
-          Logger[IO].trace(
-            s"Interpreting group ${g.show}"
-          ) *> g.expression.interpret
-        case u: Expression.Unary   =>
-          Logger[IO].trace(s"Interpreting unary ${u.show}") *> {
-            u.operator match
-              case Token.SingleCharacter.Minus =>
-                u.right.interpret.map(_.flatMap { result =>
-                  result match
-                    case dbl: Double => Right(-dbl)
-                    case v           =>
-                      Left(
-                        InterpreterError.UnaryCastError(
-                          v,
-                          Token.SingleCharacter.Minus,
+final class ExpressionInterpreter[F[_]: Sync]
+    extends Interpreter[F, Expression]
+    with Logging:
+  extension (expr: Expression)
+    def interpret: F[Either[InterpreterError, LoxValue]] =
+      withLogger {
+        import cats.implicits.*
+        expr match
+          case l: Expression.Literal =>
+            Logger[F]
+              .trace(s"Interpreting literal ${l.show}")
+              .as(Right(l.value))
+          case g: Expression.Group   =>
+            Logger[F].trace(
+              s"Interpreting group ${g.show}"
+            ) *> g.expression.interpret
+          case u: Expression.Unary   =>
+            Logger[F].trace(s"Interpreting unary ${u.show}") *> {
+              u.operator match
+                case Token.SingleCharacter.Minus =>
+                  u.right.interpret.map(_.flatMap { result =>
+                    result match
+                      case dbl: Double => Right(-dbl)
+                      case v           =>
+                        Left(
+                          InterpreterError.UnaryCastError(
+                            v,
+                            Token.SingleCharacter.Minus,
+                          )
                         )
-                      )
-                })
-              case Token.SingleCharacter.Bang  => ??? // todo
-          }
-        case _                     => ??? // todo exhaustive match
+                  })
+                case Token.SingleCharacter.Bang  => ??? // todo
+            }
+          case _                     => ??? // todo exhaustive match
+      }
 
-      def interpret: IO[Either[InterpreterError, LoxValue]] =
-        withLogger(impl)
+object Interpreter:
+  given Interpreter[IO, Expression] = new ExpressionInterpreter[IO]
